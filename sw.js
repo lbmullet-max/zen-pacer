ec1af5ea68454b25dab8bdccb50dfa43b40e2d93
lbmullet-max <lbmullet@gmail.com>
2026-05-08 10:55:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
-// This is the Service Worker that allows the app to be installed on Android
+const CACHE_NAME = 'zen-pacer-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/manifest.json'
+];
+
+// Install event: cache the files
 self.addEventListener('install', (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
+// Fetch event: serve from cache if available, otherwise go to network
 self.addEventListener('fetch', (event) => {
-  // This satisfies the "offline-capable" check
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
