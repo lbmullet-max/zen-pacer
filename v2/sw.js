898d7c16862a16e03394f1b383ebfcf849252fd9
Luke Mullet <lbmullet@gmail.com>
2026-05-30 17:43:01 -0400

    Add Breathing is Beautiful v2 with enhanced UI and meditation features

---
+++ v2/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'breath-beautiful-v2';
+const ASSETS = ['/', '/index.html', '../icon.png', '/manifest.json'];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(r => r || fetch(event.request))
+    );
+});
