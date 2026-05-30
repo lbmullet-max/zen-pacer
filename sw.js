3e9456ca62af40b5f73166a4d275fc1537ef2a19
lbmullet-max <lbmullet@gmail.com>
2026-05-30 17:55:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,15 @@
-const CACHE_NAME = 'zen-pacer-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/icon.png',
-  '/manifest.json'
-];
+const CACHE_NAME = 'breath-beautiful-v2';
+const ASSETS = ['/', '/index.html', '../icon.png', '/manifest.json'];
 
-// Install event: cache the files
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-// Fetch event: serve from cache if available, otherwise go to network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(r => r || fetch(event.request))
+    );
 });
